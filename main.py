aa881a3f3b064973ce2b5b452518bb25da70def5
umidjanovnaa <sheraliyevaruxshona08@gmail.com>
2024-02-16 20:29:00 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,53 @@
+# lst = [1,2,3,5]
+# rezalt = 0
+# for x in lst:
+#     rezalt = rezalt + x
+# print(rezalt)
+#
+# lst1 = [1,2,3,4,5]
+# x1 = min(lst1)
+# print(x1)
+
+# lst = [2,5,4,3,1,9]
+# [print(x*3)for x in lst]
+
+# lst = [2,5,4,3,1,1,9]
+# x = lst.count(1)
+# print(x)
+
+
+# # True = 1
+# # False = 0
+
+
+# #1-masala
+# def letter(lst, num):
+#     for x in lst:
+#         if type(x) == int:
+#             num.append(x)
+#     return num
+#
+# print(letter([1, 2, 3, "a", "b", 4], []))
+
+# # 2-masala
+# def indx(add_index, numbers):
+#     for num, index in enumerate(add_index):
+#         rezalt = num + index
+#     return rezalt
+# print(indx([1, 2, 3, 4, 5], []))
+
+# # 3-masala
+# def num(number):
+#     first = number[0]
+#     middle = number[1: -1]
+#     end = number[-1]
+#     return first, end
+# print(num([1, 2, 3, 4, 5, 6]))
+
+# # 4-masala
+# def cancul(lst, lst1):
+#     for x in lst:
+#         if 6 < x:
+#            lst1.append(x)
+#     return lst1
+# print(cancul([7, 4, 17, 14, 12, 3], []))
